a9bb66d47b41ef12d1cf107344b602960f723b46
Dhiyaaul Auliyaa <Dhiyaaul.Auliyaa@tvsmotor.com>
2025-09-15 23:02:08 +0700

    🛠️ fix build failed on ios

--- rs_whisper_gpt/build.rs
+++ rs_whisper_gpt/build.rs
@@ -1,4 +1,17 @@
 fn main() {
+    let target = std::env::var("TARGET").unwrap_or_default();
+    
     println!("cargo:rustc-link-lib=framework=Metal");
     println!("cargo:rustc-link-lib=framework=Foundation");
+    
+    // Link C++ standard library for iOS
+    if target.contains("ios") {
+        println!("cargo:rustc-link-lib=c++");
+        // Set iOS deployment target
+        if target.contains("simulator") {
+            println!("cargo:rustc-env=IPHONEOS_DEPLOYMENT_TARGET=12.0");
+        } else {
+            println!("cargo:rustc-env=IPHONEOS_DEPLOYMENT_TARGET=12.0");
+        }
+    }
 }
